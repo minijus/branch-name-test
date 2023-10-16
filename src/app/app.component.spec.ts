72f052474ac862d6cfe705752226e640d92e46c1
minijus <3633549+minijus@users.noreply.github.com>
2023-10-16 15:14:47 +0300

    chore: verify CI run

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome branch-name-test'
+      'Verify CI run for branch that contains forward slash'
     );
   });
 
